4fc02fdbee95b11dbb4db27074e85c75336789f3
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-05-06 14:07:00 +0300

    fix section burger

--- app/js/module.js
+++ app/js/module.js
@@ -1,1 +1,22 @@
 console.log('Привет мир');
+// Burger Carousel
+$(document).ready(function () {
+  $('.next').click(function() {
+    var currentImage = $('.burger__carousel.current');
+    var currentImageIndex  = $('.burger__carousel.current').index();
+    var nextImageIndex = currentImageIndex + 1;
+    var nextImage = $('.burger__carousel').eq(nextImageIndex);
+    currentImage.fadeOut(1000);
+    currentImage.removeClass('current');
+
+    if (nextImageIndex == ($('.burger__carousel:last').index()+1)){
+      $('.burger__carousel').eq(0).fadeIn(1000);
+      $('.burger__carousel').eq(0).addClass('current');
+    } else {
+      nextImage.fadeIn(1000);
+      nextImage.addClass('current');
+
+    };
+  });
+
+});
