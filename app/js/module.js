15b419bfab93520aa44f458737b9ee9c24f65cdc
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-05-19 23:22:42 +0300

    add pop up and ingridients js

--- app/js/module.js
+++ app/js/module.js
@@ -1,6 +1,6 @@
 // VARIABLES
 // adjustment
-const btn__close = document.querySelectorAll('.btn__close');
+const btn__close = document.querySelector('.btn__close');
 // section --do--
 // --pop up--
 const sidebar__mobile = document.querySelector('.nav-sidebar__mobile');
@@ -11,6 +11,9 @@ const btnIngridients = document.querySelector('.block__float-ingridients');
 const floatIngridients = document.querySelector('.float-ingridients__list');
 const btn__prev = document.querySelector('.btn__prev');
 const btn__next = document.querySelector('.btn__next');
+const ingridients__btn__close = document.querySelector('.ingridients__btn__close');
+// section --Team--
+const team__item = document.querySelectorAll('.team__accordeon__item');
 
 // CYCLES
 // cycle of --pop up-- menu__link
@@ -23,27 +26,44 @@ for (let i = 0; i < menu__link.length; i++) {
     transfer__menu.style.display = 'none';
   });
 };
-// cycle of --close btn-- sections do, burger
-for (let i = 0; i < btn__close.length; i++) {
-  const close = btn__close[i];
-  close.addEventListener ('click', function (e) {
-    sidebar__mobile.style.display = 'flex';
-    transfer__menu.style.display = 'none';
-    floatIngridients.style.left = '-9999px';
+// cycle of --close btn-- sections do, burger -- have a bug
+// for (let i = 0; i < btn__close.length; i++) {
+//   const close = btn__close[i];
+//   close.addEventListener ('click', function (e) {
+//     sidebar__mobile.style.display = 'flex';
+//     transfer__menu.style.display = 'none';
+//     floatIngridients.style.left = '-9999px';
+//   });
+// };
+// cycle of --team person--
+for (let i = 0; i < team__item.length; i++) {
+  const item_team = team__item[i];
+  let classItemName = item_team.className;
+  
+  item_team.addEventListener('click', function (e) {
+    e.preventDefault();
+    item_team.classList.add('team_accordeon_item_active');
+    // item_team.sublings();
+    // item_team.classList.remove('team_accordeon_item_active');
+    // console.log(classItemName);
   });
 };
 // independent EVENTS
 // --pop up-- Event
 sidebar__mobile.addEventListener('click', function (event) {
   event.preventDefault();
-  sidebar__mobile.style.display = 'none';
   transfer__menu.style.display = 'flex';
+});
+btn__close.addEventListener('click', function (e) {
+    transfer__menu.style.display = 'none';
 });
 // ingridients Event
 btnIngridients.addEventListener('click', function (e) {
   floatIngridients.style.left = '0';
 });
-
+ingridients__btn__close.addEventListener('click', function (e) {
+  floatIngridients.style.left = '-9999px';
+});
 // example pop up two --isn't work--
 // const sidebar__mobile = document.querySelector('.nav-sidebar__mobile');
 // const template = document.querySelector('#overlayTemplate').innerHTML;
