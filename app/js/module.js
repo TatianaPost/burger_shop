ff3dda972acfc44fea18fe6be51579f14dd944cc
Dmitriy_Egorov <egorov-by-egorov@mail.ru>
2018-05-22 12:07:41 +0300

    started finish project

--- app/js/module.js
+++ app/js/module.js
@@ -22,19 +22,11 @@ for (let i = 0; i < menu__link.length; i++) {
   link.addEventListener ('click', function (e) {
     e.preventDefault();
     console.log(link[i]);
-    transfer__menu.style.left = '-9999px';
+    transfer__menu.style.top = '-9999px';
     transfer__menu.style.opacity = '0';
   });
 };
-// cycle of --close btn-- sections do, burger -- have a bug
-// for (let i = 0; i < btn__close.length; i++) {
-//   const close = btn__close[i];
-//   close.addEventListener ('click', function (e) {
-//     sidebar__mobile.style.display = 'flex';
-//     transfer__menu.style.display = 'none';
-//     floatIngridients.style.left = '-9999px';
-//   });
-// };
+
 // cycle of --team person--
 for (let i = 0; i < team__item.length; i++) {
   const item_team = team__item[i];
@@ -52,12 +44,10 @@ for (let i = 0; i < team__item.length; i++) {
 // --pop up-- Event
 sidebar__mobile.addEventListener('click', function (event) {
   event.preventDefault();
-  transfer__menu.style.left = '0px';
-  transfer__menu.style.opacity = '1';
+  transfer__menu.style.right = '0px';
 });
 btn__close.addEventListener('click', function (e) {
-  transfer__menu.style.left = '-9999px';
-  transfer__menu.style.opacity = '0';
+  transfer__menu.style.right = '-9999px';
 });
 // ingridients Event
 btnIngridients.addEventListener('click', function (e) {
@@ -69,33 +59,6 @@ ingridients__btn__close.addEventListener('click', function (e) {
   floatIngridients.style.opacity = '0';
   // floatIngridients.style.left = '-9999px';
 });
-// example pop up two --isn't work--
-// const sidebar__mobile = document.querySelector('.nav-sidebar__mobile');
-// const template = document.querySelector('#overlayTemplate').innerHTML;
-// const overlay = createOverlay(template);
-
-// sidebar__mobile.addEventListener('click', function() {
-//   overlay.open();
-
-// });
-
-// function createOverlay(template) {
-//   let fragment = document.createElement('div');
-
-//   fragment.innerHTML = template;
-
-//   const transfer__menu = fragment.querySelector('.mobile__transfer__menu');
-//   const btn__close = fragment.querySelector('.btn__close');
-
-//   fragment = null;
-
-//   btn__close.addEventListener('click', function (e) {
-    
-//   });
-
-// };
-
-// section --burger--
 
 
 // --Form--
